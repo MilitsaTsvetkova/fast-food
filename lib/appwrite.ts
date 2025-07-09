c32b334faa519e742ea6b5266b8b2f514744d7c2
MilitsaTsvetkova <militsa88@yahoo.com>
2025-07-09 08:42:08 +0300

    appwrite config

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: "comm.scooby.fooddelivery",
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
